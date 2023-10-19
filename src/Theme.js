1c03c02c15a1c19cf2bd2a7189dd04e231b275ce
Kamzen <kamzen1994@gmail.com>
2023-10-19 20:52:00 +0200

    Create Theme.js

---
+++ src/Theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const Theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#008d8a"
+    }
+  },
+  typography: {
+    fontSize: 12
+  }
+});
+
+export default Theme;
