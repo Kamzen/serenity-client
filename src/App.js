ddad92747e3939b23f1b2f10fd8e1c7838185d42
Kamzen <kamzen1994@gmail.com>
2023-10-19 20:54:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import Theme from "./Theme";
+import DesktopNavigation from "./components/Navigation/DesktopNavigation";
+import LoginUserWithPassword from "./pages/Auth/LoginUserWithPassword";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<DesktopNavigation />} />
+          <Route
+            path="/loginWithPassword"
+            element={<LoginUserWithPassword />}
+          />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
